2dd736fb50d9cdac420baab911cb6e06f77d365c
MarkusHuang <hwangjing1022@gmail.com>
2025-11-17 20:52:12 +0100

    add single agent

---
+++ agents/single_search_agent/agent.py
@@ -0,0 +1,13 @@
+from google.adk.agents import Agent
+from google.adk.tools import google_search
+
+# Prompt -> Agent -> Thought -> Action -> Observation -> Final Answer
+
+
+root_agent = Agent(
+    name="search_agent",
+    model="gemini-2.5-flash-lite",
+    description="An agent that can perform Google searches to answer questions.",
+    instruction="You are an agent that can perform Google searches to find information and answer questions.",
+    tools=[google_search]
+    )
